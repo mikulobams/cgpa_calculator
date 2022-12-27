5f35b3a7a7c451ad0b33289bb1bde5fa9a3284cd
Michael Bamikunle <mikulobams@gmail.com>
2022-12-27 19:39:43 +0100

    sqlalchemy 1st version

--- student_class.py
+++ student_class.py
@@ -1,15 +1,35 @@
-class Student:
+from sqlalchemy import create_engine, ForeignKey, Column, String, Integer, CHAR, REAL
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from subject_class import *
+
+Base = declarative_base()
+
+
+
+class Student(Base):
+    __tablename__ = "student_records"
+
+    regno = Column("regno" , String, primary_key = True)
+    fullname = Column("fullname", String)
+    tcur = Column('tcur', REAL)
+    tcue = Column('tcue', REAL)
+    cgpa = Column('cgpa', REAL)
+    remarks = Column('remarks', String)
+
+
+    
     """defines instances of students"""
     def __init__(self, std_full_name, reg_no):
-        self.std_full_name = std_full_name.upper()
-        self.reg_no = reg_no.upper()
+        self.fullname = std_full_name.upper()
+        self.regno = reg_no.upper()
         self.tcur = 0
         self.tcue = 0
         self.cgpa = 0
-
+        self.remarks = ''
     def calculate_cgpa(self, subject, score):
         self.tcur += subject.credit_unit
-        
+      
         if score >= 70:
             grade_point = 4.00
         elif score >= 65:
@@ -22,11 +42,29 @@ class Student:
             grade_point = 2.00
         else:
             grade_point = 0.00
+            self.remarks += subject.course_title
         
         cue = float(grade_point * subject.credit_unit)
         self.tcue += cue
         self.cgpa = round(self.tcue/self.tcur, 2)
+    def __repr__(self):
+        return f"({self.fullname}) ({self.regno}) ({self.tcur}) ({self.tcue}) ({self.cgpa}) ({self.remarks})"
         
 
+engine = create_engine("sqlite:///test.db", echo = True)
 
+Base.metadata.create_all(bind=engine)
 
+Session = sessionmaker(bind=engine)
+session = Session()
+
+student_1 = Student('Michael Bamikunle', 'u09ph1055')
+
+student_1.calculate_cgpa(bio_111, 60)
+student_1.calculate_cgpa(aum_321, 71)
+student_1.calculate_cgpa(bdt_152, 40)
+student_1.calculate_cgpa(hcp_361, 70)
+student_1.calculate_cgpa(bio_112, 70)
+
+session.add(student_1)
+session.commit()
